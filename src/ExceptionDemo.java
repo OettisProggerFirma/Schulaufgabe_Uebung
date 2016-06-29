6e1f07c76f7e7d419d665d3bde848fcb6d738740
Denis Oetken <denisoetken@hotmail.de>
2016-06-29 14:22:33 +0200

    IML hinzugefügt

--- src/ExceptionDemo.java
+++ src/ExceptionDemo.java
@@ -1,12 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by doetken on 29.06.2016.
  */
 public class ExceptionDemo {
 
     public static void main(String[] args) {
+
+//      nicht zu behandelnder Programmierfehler:
         System.out.println("Vor Fehler");
-        System.out.println(""+0/0);
+        System.out.println("" + 0 / 0);
         System.out.println("Nach Fehler");
+        try (BufferedReader bfr = new BufferedReader(new FileReader("src/ExceptionDemo.java"))) {
+            String zeile = "";
+            while ((zeile = bfr.readLine()) != null) {
+                System.out.println(zeile);
+            }
+//            Datei nicht gefunden
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+//            in der Regel während des Lesens
+        } catch (IOException e) {
+            e.printStackTrace();
+        }}
+//        Methode verlangt Fehlerbehandlung:
+
+        public void methode ()throws IOException {
+        }
     }
 
-}
